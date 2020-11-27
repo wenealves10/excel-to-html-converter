eb08014a9cd6c921f110b8adc7acc11e51874dfc
Wene Alves <weneplay5@gmail.com>
2020-11-27 09:00:55 -0300

    Starting the Data Table

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const Reader = require('../Reader/Reader')
 const dataManipulation = require('../Data-manipulation/DataManipulation')
+const Table = require('../Table/Table')
 
 const read = new Reader()
 
 let date = read.read(__dirname+'/../escola.csv')
 let databases = dataManipulation.Process(date)
+const list = new Table(databases)
 
-console.log(databases)
