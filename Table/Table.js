a62cd9bc4732649ce5aba30876e3a3196f52f78a
Wene Alves <weneplay5@gmail.com>
2020-11-28 07:59:58 -0300

    Virtual fields with get

--- Table/Table.js
+++ Table/Table.js
@@ -4,6 +4,14 @@ class Table{
         date.shift()
         this.body = date
     }
+
+    get CountRow(){
+        return this.body.length
+    }
+
+    get CountColumn(){
+        return this.header.length
+    }
 }
 
 module.exports = Table;
