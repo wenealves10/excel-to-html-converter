4524178e0bda5571d19fd1ef8539f07ce58c2d2d
Wene Alves <weneplay5@gmail.com>
2020-11-28 09:44:58 -0300

    Instantiating the pdf generator

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const dataManipulation = require('../Data-manipulation/DataManipulation')
 const Table = require('../Table/Table')
 const EjsParser = require('../EjsParser/EjsParser')
 const HtmlGenerator = require('../HtmlGenerator/HtmlGenerator')
+const PdfGenerator = require('../PdfGenerator/PdfGenerator')
 async function main() {
     const read = new Reader()
     let date = read.read(__dirname + '/../escola.csv')
@@ -10,6 +11,7 @@ async function main() {
     const list = new Table(databases)
     const html = await EjsParser.Parser(list)
     HtmlGenerator.Generate(html)
+    PdfGenerator.Generate(Date.now()+'table.pdf',html)
 }
 
 main()
