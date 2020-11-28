5997baa247db7867ac8d8487a7a21d495c1961f7
Wene Alves <weneplay5@gmail.com>
2020-11-28 09:12:38 -0300

    Instantiating the Html Generator

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const Reader = require('../Reader/Reader')
 const dataManipulation = require('../Data-manipulation/DataManipulation')
 const Table = require('../Table/Table')
 const EjsParser = require('../EjsParser/EjsParser')
+const HtmlGenerator = require('../HtmlGenerator/HtmlGenerator')
 async function main() {
     const read = new Reader()
     let date = read.read(__dirname + '/../escola.csv')
     let databases = dataManipulation.Process(date)
     const list = new Table(databases)
     const html = await EjsParser.Parser(list)
-    console.log(html)
+    HtmlGenerator.Generate(html)
 }
 
 main()
